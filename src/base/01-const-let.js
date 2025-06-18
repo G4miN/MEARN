ed3e81501a9c4cc9bcdd35306a02c73f3c827a6c
yeduin <el_yeduin_02@hotmail.com>
2025-06-18 15:37:19 -0400

    working with const and let

---
+++ src/base/01-const-let.js
@@ -0,0 +1,13 @@
+//! Variables and constants
+const name = "Yeduin";
+let lastName = "Mateo";
+
+console.log(name, lastName)
+
+if(true){
+  const name = "Pepe";
+  lastName = "Madavi"
+  console.log(name)
+}
+
+console.log(name, lastName);
